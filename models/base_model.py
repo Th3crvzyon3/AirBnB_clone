c573cbc301933302aa334d2ba8e08a50409cab8b
Abraham Otieno <abrahamlincoln8212gmail.com>
2022-08-07 22:44:47 +0300

    Fix error

--- models/base_model.py
+++ models/base_model.py
@@ -1,53 +1,47 @@
 #!/usr/bin/python3
-""" Module for Base """
+"""File containing BaseModel Class"""
 
-import uuid
 from datetime import datetime
-from uuid import uuid4
 import models
-import json
-
-format_dt = "%Y-%m-%dT%H:%M:%S.%f"
+import uuid
 
 
 class BaseModel:
-    """ Basemodel class """
+
     def __init__(self, *args, **kwargs):
-        """ Initialization of Database """
-        if args is not None and len(args) > 0:
-            pass
-        if kwargs:
-            for key, item in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    item = datetime.strptime(item, format_dt)
-                if key not in ['__class__']:
-                    setattr(self, key, item)
-        else:
+        """Initialize a new Base Model"""
+        if kwargs is None or len(kwargs) == 0:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             models.storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    self.__dict__[key] = datetime\
+                        .strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
 
-    def to_dict(self):
-        """ to_dict definition """
-        
-        dic = {}
-        for key, item in self.__dict__.items():
-            if key in ['created_at', 'updated_at']:
-                dic[key] = item
-
-        dic['__class__'] = self.__class__.__name__
-        dic['created_at'] = self.created_at.isoformat()
-        dic['updated_at'] = self.updated_at.isoformat()
-        return dic
-
+                elif key == "id":
+                    self.id = value
+                elif key == "__class__":
+                    self.__class__.__name__ = kwargs[key]
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
-        """ str definition """
-        return("[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__))
+        """String representation of the Base Model"""
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
 
     def save(self):
-        """ Save definition """
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+        """Updates updated_at with the current time"""
+        self.updated_at = datetime.utcnow()
         models.storage.save()
+
+    def to_dict(self):
+        """Creates a dictionary of BaseModel"""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = datetime.isoformat(self.created_at)
+        my_dict["updated_at"] = datetime.isoformat(self.updated_at)
+        return my_dict
